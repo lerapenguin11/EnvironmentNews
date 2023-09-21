d464258dd0b3125f3af0c44a9043635db653ae9b
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-09-21 15:42:23 +0500

    project setup

--- app/src/main/java/com/example/environmentnews/MainActivity.kt
+++ app/src/main/java/com/example/environmentnews/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.environmentnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.environmentnews.databinding.ActivityMainBinding
+import com.example.environmentnews.utilits.APP_ACTIVITY
+import com.example.environmentnews.utilits.setStatusBarGradiantMain
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        APP_ACTIVITY = this
+        setStatusBarGradiantMain(this)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/environmentnews/utilits/Const.kt
@@ -0,0 +1,5 @@
+package com.example.environmentnews.utilits
+
+import com.example.environmentnews.MainActivity
+
+lateinit var APP_ACTIVITY : MainActivity
